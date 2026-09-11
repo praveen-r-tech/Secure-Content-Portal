22f4a8ebc5348bb3e916e5218e0ea5052651d25d
praveen-r-tech <rajupraveen.2005@gmail.com>
2026-09-11 21:54:24 +0530

    Improve UI and error handling

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -1,5 +1,10 @@
 function Loading() {
-  return <div className="page-loading">Loading…</div>
+  return (
+    <div className="page-loading">
+      <div className="loading-spinner" />
+      Loading…
+    </div>
+  )
 }
 
 export default Loading
